bf4eb8495f2f9e390ddeefc2a2d9fd0da699588d
Akhilteja1 <43044930+Akhilteja1@users.noreply.github.com>
2023-09-13 16:29:35 +0000

    displayed the selected emoji

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
- 
+import React,{useState} from 'react';
+import data from "@emoji-mart/data";
 import Picker from "@emoji-mart/react";
 
 
 const App = () => {
+  const [showEmoji, setShowEmoji] = useState(false);
+  const [textValue, setTextValue] = useState("");
+
+  const addEmoji =(e)=>{
+    const sym = e.unified.split("_");
+    const codeArray = [];
+    sym.forEach((el) => codeArray.push("0x" + el));
+    let emoji = String.fromCodePoint(...codeArray);
+    setTextValue(emoji);
+  }
   
    
 
   return (
     <div>
-      
-      <Picker   />
+      <button onClick={()=>setShowEmoji(!showEmoji)}>
+        add emoji
+      </button>
+      {showEmoji && (
+        <Picker data ={data} onEmojiSelect={addEmoji}  />)
+        }
       <br/>
+      <p >{textValue}</p>
        
     </div>
   );
